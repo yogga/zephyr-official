f25aa9efbe86355a7a5f6048a27b74b44e981645
arifyogaw <septiandolo@gmail.com>
2024-10-16 20:27:54 +0700

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,12 @@ export default function RootLayout({
   return (  
     <html lang="en">  
       <head>  
-        {/* Favicon Links */}  
-        <link rel="icon" href="/logo-zephyr.png" sizes="32x32" />  
-        <link rel="icon" href="/logo-zephyr.png" sizes="192x192" />  
-        <link rel="apple-touch-icon" href="/logo-zephyr.png" />  
+         
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" /> 
         <meta name="theme-color" content="#ffffff" />  
       </head>  
       <body 
